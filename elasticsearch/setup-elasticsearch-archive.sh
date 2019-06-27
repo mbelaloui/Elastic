4913e98806a3b04a2703068cd59ab99bdaccceac
datafab <mbelalou@student.42.fr>
2019-06-27 15:59:43 +0200

    fix heap mim value elasticsearch

--- elasticsearch/setup-elasticsearch-archive.sh
+++ elasticsearch/setup-elasticsearch-archive.sh
@@ -1,3 +1,7 @@
 curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.2.0-linux-x86_64.tar.gz
 tar -xvf elasticsearch-7.2.0-linux-x86_64.tar.gz
-cp elastic_conf.yml elasticsearch-7.2.0/config/elasticsearch.yml
+###########################################################3
+# NB it's not the safe way to run elasticsearch it's just for test
+#echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+#sysctl -w vm.max_map_count=262144
+#cp elastic_conf.yml elasticsearch-7.2.0/config/elasticsearch.yml
